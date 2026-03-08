562ff2b9f34cdeb9235d6572e75c39d8625f3ab8
Person Boy <170485328+ZeldaCool@users.noreply.github.com>
2026-03-08 17:50:40 -0600

    feedback audio loop

---
+++ src-tauri/src/audio/mod.rs
@@ -0,0 +1,51 @@
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use ringbuf::{
+    traits::{Consumer, Producer, Split},
+    HeapRb,
+};
+
+
+pub fn err_fn(err: cpal::StreamError) {
+    eprintln!("an error occurred on stream: {err}");
+}
+
+pub fn audio_loop() -> anyhow::Result<()>  {
+    let host = cpal::default_host();
+    let inputdev = host.default_input_device();
+    let outputdev = host.default_output_device();
+
+    //let inputconf: cpal::StreamConfig = inputdev.expect("failed to get input device.").default_input_config()?.into();
+    //let outputconf: cpal::StreamConfig = outputdev.expect("failed to get input device.").default_output_config()?.into();
+
+    let config: cpal::StreamConfig = inputdev.clone().expect("failed to get device(s).").default_input_config()?.into();
+
+    let ring = HeapRb::<f32>::new(48000);
+    let (mut producer, mut consumer) = ring.split();
+
+
+    let inputfn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
+         for &sample in data {
+            producer.try_push(sample);
+        }
+    };
+
+    let outputfn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+        for sample in data {
+            *sample = consumer.try_pop().unwrap_or(0.0);
+        }
+    };
+
+    let input_stream = inputdev.as_ref().expect("Got Option::None").build_input_stream(&config, inputfn, err_fn, None)?;
+    let output_stream = outputdev.as_ref().expect("Got Option::None").build_output_stream(&config, outputfn, err_fn, None)?;
+
+    input_stream.play()?;
+    output_stream.play()?;
+
+    println!("Playing for 10 seconds... ");
+    std::thread::sleep(std::time::Duration::from_secs(10));
+    drop(input_stream);
+    drop(output_stream);
+    println!("Done!");
+
+    Ok(())
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    murmr_lib::run()
+//fn main() {
+//    murmr_lib::run()
+//}
+//
+
+mod audio;
+use tokio::sync::mpsc::{Sender, Receiver};
+use crate::audio::audio_loop; 
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+        audio_loop()?;
+
+        Ok(())
 }
